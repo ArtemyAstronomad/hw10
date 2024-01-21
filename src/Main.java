e65ba7aabdeee49299c65d2afff61773b3d3017d
ArtemyAstronomad <op2607@yandex.ru>
2024-01-22 00:02:22 +0300

    Initital commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+import java.util.Random;
+public class Main {
+
+    public static void main(String[] args) {
+task1();
+    }
+    private static void task1(){
+
+    }
+}
