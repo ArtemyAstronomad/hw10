3e9d52fc962f3c66794b7493da2dc3828a23464c
ArtemyAstronomad <op2607@yandex.ru>
2024-01-22 01:17:02 +0300

    Done

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,34 @@
-import java.util.Arrays;
-import java.util.Random;
+
 public class Main {
 
     public static void main(String[] args) {
 task1();
+task2();
+task3();
+
     }
     private static void task1(){
+    String firstName = " Ivan ";
+    String middleName = " Ivaniv ";
+    String lastName =" Ivanovich ";
+    String fullName  = firstName + middleName + lastName;
+        System.out.println("Ф.И.О. сотрудника - " + fullName);
+    }
 
+    private static void task2(){
+        String firstName = " Ivan ";
+        String middleName = " Ivaniv ";
+        String lastName =" Ivanovich ";
+        String fullName  = firstName + middleName + lastName;
+        System.out.println("Ф.И.О. сотрудника для заполнения отчета - " + fullName.toUpperCase());
+
+    }
+    private static void task3() {
+        String firstName = " Семён ";
+        String middleName = " Семенов ";
+        String lastName = " Семенович ";
+        String fullName = firstName + middleName + lastName;
+        fullName = fullName.replace('ё', 'е');
+        System.out.println("Ф.И.О. сотрудника для заполнения отчета - " + fullName.toUpperCase());
     }
 }
